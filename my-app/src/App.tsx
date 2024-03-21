92498115d342998f7192bf4b840377edcc005e15
“[Mocanu <lmocanu504@gmail.com>
2024-03-21 12:36:19 +0200

    Am modificat fisierul App.tsx

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
+  const firstName = "Mocanu";
+  const lastName = "Loredana";
+  const groupData = "CR-221";
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Bine ai venit, {firstName} {lastName}</h1>
+        <p>Grupa: {groupData}</p>
       </header>
     </div>
   );
-}
+};
 
 export default App;
